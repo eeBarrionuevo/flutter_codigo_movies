91c07f47da3417bd935a5db4b9a3444cc6b01f2e
Elvis Barrionuevo <idcareg.g@gmail.com>
2021-03-25 20:40:19 -0500

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage("https://image.freepik.com/free-vector/colorful-abstract-wallpaper-design_23-2148467625.jpg")
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      "https://images.pexels.com/photos/845457/pexels-photo-845457.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    ),
+                    radius: 30,
+                  ),
+                  Text("Benito Lopez Guzman", style: TextStyle(fontSize: 18.0),),
+                  Text("Administrador", style: TextStyle(fontSize: 12.0, color: Colors.white),),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
