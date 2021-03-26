17927be5522223de7ce8449db02dd9b6f37088d1
Elvis Barrionuevo <idcareg.g@gmail.com>
2021-03-25 22:05:06 -0500

    ListView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,25 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo_movies/widgets/drawer_widget.dart';
+import 'package:http/http.dart' as http;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var moviesList;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("Iniciando vista Home...");
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,9 +35,7 @@ class HomePage extends StatelessWidget {
       drawer: DrawerWidget(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.all_inclusive),
-        onPressed: (){
-
-        },
+        onPressed: () {},
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomNavigationBar(
@@ -34,6 +50,42 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              "Top Populares",
+              style: TextStyle(
+                  fontSize: 35.0,
+                  color: Theme.of(context).accentColor,
+                  fontWeight: FontWeight.w500),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: moviesList == null ? 0 : moviesList.length,
+              itemBuilder: (context, index) {
+                return Text(moviesList[index]["original_title"]);
+              },
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  Future<Map> obtenerJson() async {
+    var apiKey = "077c6b8e215715b477754b84846c0f95";
+    var url = Uri.parse(
+        'https://api.themoviedb.org/3/discover/movie?api_key=$apiKey');
+    var respuesta = await http.get(url);
+    return json.decode(respuesta.body);
+  }
+
+  void getData() async {
+    var data = await obtenerJson();
+    moviesList = data["results"];
+    setState(() {});
   }
 }
