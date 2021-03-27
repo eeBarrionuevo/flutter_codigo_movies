9a1fc48cca23afb2beba9e99968b9c9071bae803
Elvis Barrionuevo <idcareg.g@gmail.com>
2021-03-26 21:51:12 -0500

    MovieDetail 5

--- lib/pages/movie_detail_page.dart
+++ lib/pages/movie_detail_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -39,43 +40,135 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled){
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return <Widget>[
             SliverAppBar(
               pinned: false,
               floating: true,
-              snap: false ,
+              snap: false,
               expandedHeight: 220.0,
               flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                title: Text("Nombre película"),
-                background: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    Image.network("https://as02.epimg.net/meristation/imagenes/2021/03/23/noticias/1616512421_355291_1616512451_noticia_normal_recorte1.jpg",
-                    fit: BoxFit.fitWidth,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                          colors: [
-                            Colors.black54,
-                            Colors.black
-                          ]
+                  centerTitle: true,
+                  title: Text(
+                    "Nombre película",
+                    style: TextStyle(fontSize: 14.0),
+                  ),
+                  background: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Image.network(
+                        "https://as02.epimg.net/meristation/imagenes/2021/03/23/noticias/1616512421_355291_1616512451_noticia_normal_recorte1.jpg",
+                        fit: BoxFit.fitWidth,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Colors.black12.withOpacity(0),
+                                Colors.black12.withOpacity(0.5),
+                                Colors.black,
+                              ]),
                         ),
                       ),
-                    ),
-
-                  ],
-                )
-              ),
-              
+                    ],
+                  )),
             ),
           ];
         },
-        body: Center(child: Text("asdsad"),),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12.0),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              "https://images.pexels.com/photos/3866555/pexels-photo-3866555.png?auto=compress&cs=tinysrgb&dpr=1&w=500",
+                            ),
+                          )),
+                    ),
+                    Column(
+                      children: [
+                        Text("Titulo Película"),
+                        RichText(
+                          text: TextSpan(
+                            text: "2020 ",
+                            children: [
+                              TextSpan(
+                                text: " - ",
+                              ),
+                              TextSpan(
+                                text: " 120 min ",
+                              )
+                            ],
+                          ),
+                        ),
+                        Row(
+                          children: [Icon(Icons.star), Text("12323")],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [Icon(Icons.thumb_up), Text("Me gusta")],
+                    ),
+                    Column(
+                      children: [Icon(Icons.thumb_down), Text("No me gusta")],
+                    ),
+                    Column(
+                      children: [Icon(Icons.playlist_add), Text("Mi lista")],
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("Alquilar en HD por 55.00 PE", textAlign: TextAlign.center,),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("Alquilar en HD por 55.00 PE", textAlign: TextAlign.center,),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
