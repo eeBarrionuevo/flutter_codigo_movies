3d1f10129b92bb5dbdd33409d2cc47abcb662ed3
Elvis Barrionuevo <idcareg.g@gmail.com>
2021-03-26 20:50:15 -0500

    MovieDetail 2

--- lib/pages/movie_detail_page.dart
+++ lib/pages/movie_detail_page.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MovieDetailPage extends StatefulWidget {
-
   int id;
   MovieDetailPage({this.id});
 
@@ -9,13 +11,37 @@ class MovieDetailPage extends StatefulWidget {
   _MovieDetailPageState createState() => _MovieDetailPageState();
 }
 
-
 class _MovieDetailPageState extends State<MovieDetailPage> {
+  var movieDetail;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("Iniciando vista detalle...");
+    getData();
+  }
+
+  Future<Map> getJson() async {
+    var apiKey = "077c6b8e215715b477754b84846c0f95";
+    var url = Uri.parse(
+        "https://api.themoviedb.org/3/movie/${widget.id}?api_key=$apiKey");
+    var response = await http.get(url);
+    return json.decode(response.body);
+  }
+
+  getData() async {
+    movieDetail = await getJson();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Hola ${widget.id}"),
+        child: movieDetail != null
+            ? Text("Hola ${movieDetail["original_title"]}")
+            : CircularProgressIndicator(),
       ),
     );
   }
