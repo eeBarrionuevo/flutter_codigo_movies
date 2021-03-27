437ba06e8afd23c1628e9f375e71b33748d10b0f
Elvis Barrionuevo <idcareg.g@gmail.com>
2021-03-26 19:07:19 -0500

    ListView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo_movies/widgets/drawer_widget.dart';
 import 'package:http/http.dart' as http;
@@ -9,8 +10,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List moviesList;
 
-  var moviesList;
+  String image_url = "https://image.tmdb.org/t/p/w500";
 
   @override
   void initState() {
@@ -66,7 +68,68 @@ class _HomePageState extends State<HomePage> {
             child: ListView.builder(
               itemCount: moviesList == null ? 0 : moviesList.length,
               itemBuilder: (context, index) {
-                return Text(moviesList[index]["original_title"]);
+                return Row(
+                  children: [
+                    // Aquí estará la imagen
+
+                    Container(
+                      child: Container(
+                        width: 90.0,
+                        height: 90.0,
+                      ),
+                      margin: EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              image_url + moviesList[index]["poster_path"],
+                          ),
+                          fit: BoxFit.cover
+                        ),
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            blurRadius: 7,
+                            offset: Offset(2,3)
+                          ),
+                        ],
+                      ),
+
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.symmetric(horizontal: 5),
+                        child: Column(
+                          children: [
+                            Text(
+                              moviesList[index]["original_title"] ?? "",
+                              style: TextStyle(
+                                  fontSize: 18.0, fontWeight: FontWeight.w600),
+                            ),
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Row(
+                              children: [
+                                Icon(Icons.star),
+                                Text(moviesList[index]["popularity"].toString())
+                              ],
+                            ),
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Text(
+                              moviesList[index]["overview"],
+                              maxLines: 3,
+                            ),
+                          ],
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
               },
             ),
           ),
@@ -76,7 +139,8 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<Map> obtenerJson() async {
-    var apiKey = "077c6b8e215715b477754b84846c0f95";
+    String apiKey = "077c6b8e215715b477754b84846c0f95";
+
     var url = Uri.parse(
         'https://api.themoviedb.org/3/discover/movie?api_key=$apiKey');
     var respuesta = await http.get(url);
